3b57c08353f963ac32e2d3091d4cc24e579fa9f3
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2020-01-15 09:52:20 +0100

    Add station

---
+++ station.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+set -uo
+
+readonly station_path=$(find "$HOME/.local/" -name 'Station*')
+
+pgrep 'Station' > /dev/null
+result_status="$?"
+set -e
+if [ "$result_status" -gt 0 ]; then
+  if [ -f "$station_path" ]; then
+    "$station_path" &> /dev/null &
+  fi
+fi
