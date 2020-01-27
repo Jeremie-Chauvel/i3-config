89b6a28e6b0ddac0e2ea081172b9e7eb748e5e4c
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2020-01-27 09:27:31 +0100

    Add feh to installed packages

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-sudo apt install -y i3 i3-ipc i3status i3lock dmenu xautolock kitty
+sudo apt install -y i3 i3-ipc i3status i3lock dmenu xautolock kitty feh
 git clone https://github.com/hastinbe/i3-volume.git ~/.i3/i3-volume
 mkdir -p ~/.config/i3
 for file in ./*
