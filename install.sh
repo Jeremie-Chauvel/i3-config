4472fbb41bb8662ac363e850928cba41482aa89e
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2020-02-04 08:54:41 +0100

    Add qwerty layout

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ set -euo pipefail
 IFS=$'\n\t'
 
 sudo apt install -y i3 i3-ipc i3status i3lock dmenu xautolock kitty feh
+
+# keyboard layout
+curl -O http://marin.jb.free.fr/qwerty-fr/xkb-qwerty-fr_0.5_all.deb
+sudo dpkg --install ./xkb-qwerty-fr_0.5_all.deb
+
 git clone https://github.com/hastinbe/i3-volume.git ~/.i3/i3-volume
 mkdir -p ~/.config/i3
 for file in ./*
